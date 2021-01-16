ddd0c1dcb2a5552f4aa7839a2cd7e37833f9bd85
Gerry <inet_ygssoftware@163.com>
2021-01-16 18:22:40 +0800

    V4.0版本

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Zhaoxi.NET5Project.Controllers
 
         public IActionResult Index()
         {
-            base.ViewData["User1"] = "张三-1发布新版本V3.0";
+            base.ViewData["User1"] = "张三-1发布新版本V4.0";
             base.TempData["User2"] = "李四-111";
             base.ViewBag.User3 = "王五-失败";
             object User4 = "赵六111111";
